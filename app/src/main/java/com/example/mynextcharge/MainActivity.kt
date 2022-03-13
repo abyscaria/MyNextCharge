5aa3835919ad4cc78ec9fb4c4ea21eaa9eb6969b
Aby Scaria <aby.scaria@gmail.com>
2022-03-13 20:36:27 +1100

    git tutorial

--- app/src/main/java/com/example/mynextcharge/MainActivity.kt
+++ app/src/main/java/com/example/mynextcharge/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         tabs.setupWithViewPager(viewPager)
         val fab: FloatingActionButton = binding.fab
 
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
